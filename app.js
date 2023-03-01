b9dbf9976ee77980756bea1d07a56bf945bd15ea
Travless <travis.white0101@gmail.com>
2023-03-01 14:52:46 -0500

    Begin developing route handler with setting up a get request of the tours offered through the api

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-// HTTP Method for request
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello from the server side!', 
-        app: 'Natours'
-    });
-});
+// // HTTP Method for Get Request
+// app.get('/', (req, res) => {
+//     res.status(200).json({
+//         message: 'Hello from the server side!', 
+//         app: 'Natours'
+//     });
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
+// // HTTP Method for Post Request
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint...');
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// Route handler
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
 })
 
 const port = 3000;
