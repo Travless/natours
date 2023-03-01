ff11ba640c0cb301abca0930c708f7d741a061b3
Travless <travis.white0101@gmail.com>
2023-03-01 15:17:59 -0500

    Create post request to api that adds a new tour to the list of tours offered

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
+// middleware
+app.use(express.json());
+
 // // HTTP Method for Get Request
 // app.get('/', (req, res) => {
 //     res.status(200).json({
@@ -20,7 +23,9 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// Route handler
+// Route handlers
+// GET
+// Get list of tour info
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -28,6 +33,26 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
+    })
+});
+
+// POST
+// Add new tour
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 })
 
