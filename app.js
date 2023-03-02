b187aa65a0be0e31f0f5ffcbbbdff6fec14e239a
Travless <travis.white0101@gmail.com>
2023-03-02 11:07:40 -0500

    House tour and user routes in two seperate modules that can be imported as small sub apps to main app.js file as middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -23,146 +25,25 @@ app.use((req, res, next) => {
     next();
 })
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-
 
 
 
 // 2) Route handlers
-// GET
-// Get list of tour info
-const getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-};
-
-// Get tour info by id
-const getTourById = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-
-    if (!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    })
-};
-
-// POST
-// Add new tour
-const addNewTour = (req, res) => {
-
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    })
-};
-
-// PATCH
-const updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here>'
-        }
-    });
-};
-
-// DELETE
-const deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
-};
 
 
 
-const getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'Error',
-        message: 'This route is not yet defined'
-    })
-}
 
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: 'Error',
-        message: 'This route is not yet defined'
-    })
-}
-
-const getUser = (req, res) => {
-    res.status(500).json({
-        status: 'Error',
-        message: 'This route is not yet defined'
-    })
-}
-
-
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'Error',
-        message: 'This route is not yet defined'
-    })
-}
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'Error',
-        message: 'This route is not yet defined'
-    })
-}
 
 // 3) ROUTES
 
-const tourRouter = express.Router();
-const userRouter = express.Router();
+// const tourRouter = express.Router();
+// const userRouter = express.Router();
 
-tourRouter.route('/').get(getAllTours).post(addNewTour);
-tourRouter.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
+// tourRouter.route('/').get(getAllTours).post(addNewTour);
+// tourRouter.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
 
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+// userRouter.route('/').get(getAllUsers).post(createUser);
+// userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
