4b6ad1930181aeb9fef426174dd612c22999c7f5
Travless <travis.white0101@gmail.com>
2023-03-02 11:54:53 -0500

    Create user and tour controller files to import to route files for requests

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,97 +1,15 @@
-const fs = require('fs');
 const express = require('express');
-
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-
-// GET
-// Get list of tour info
-const getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-};
-
-// Get tour info by id
-const getTourById = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-
-    if (!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    })
-};
-
-// POST
-// Add new tour
-const addNewTour = (req, res) => {
-
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    })
-};
-
-// PATCH
-const updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here>'
-        }
-    });
-};
-
-// DELETE
-const deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
-};
+const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(addNewTour);
-router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
+router.route('/')
+    .get(tourController.getAllTours)
+    .post(tourController.addNewTour);
+
+router.route('/:id')
+    .get(tourController.getTourById)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour);
 
 module.exports = router;
