3f3c4d0057562459003aca2b343ac2f04850c49b
Travless <travis.white0101@gmail.com>
2023-03-02 12:16:11 -0500

    Export seperated checkID function from request functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is ${val}`);
+    if (req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+};
+
 
 // GET
 // Get list of tour info
@@ -23,13 +34,6 @@ exports.getAllTours = (req, res) => {
 exports.getTourById = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    if (!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
 
     res.status(200).json({
         status: 'success',
@@ -60,12 +64,6 @@ exports.addNewTour = (req, res) => {
 
 // PATCH
 exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(200).json({
         status: 'success',
         data: {
@@ -76,12 +74,6 @@ exports.updateTour = (req, res) => {
 
 // DELETE
 exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(204).json({
         status: 'success',
         data: null
