a14db78e45ba167d1971d7947aba76548ca5c54f
Travless <travis.white0101@gmail.com>
2023-03-07 09:58:06 -0500

    Impliment pagination filtering within requests via page numbers and limits of documents within pages

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -48,13 +48,35 @@ exports.getAllTours = async (req, res) => {
     let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
-    if(req.query.sort) {
+    if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
       // sort('price ratings')
     } else {
       query = query.sort('-createdAt');
     }
+
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10, 1-10 = page 1, 11-20 = page 2, etc.
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exisit');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
--- models/tourModel.js
+++ models/tourModel.js
@@ -49,8 +49,9 @@ const tourSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now(),
+    select: false,
   },
-  dtartDates: [Date],
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
