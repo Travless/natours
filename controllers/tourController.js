3c169cf12545faea3c23b2dd5b89759d03b5b2bb
Travless <travis.white0101@gmail.com>
2023-03-07 09:06:33 -0500

    Create query filtering method that matches the returned object based on additional filtering measures such as greater than or equal, greater than, less than or equal, and less than

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,23 +34,28 @@ const Tour = require('./../models/tourModel');
 // Get list of tour info
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    // 2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(queryObj);
+
+    const query = Tour.find(JSON.parse(queryStr));
+    // EXECUTE QUERY
+    const tours = await query;
 
     // const query = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXECUTE QUERY
-    const tours = await query;
 
     // SEND QUERY
     res.status(200).json({
