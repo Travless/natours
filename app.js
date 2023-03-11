01d54f36f6cf2d9d98f618384d47dad9d7c91da4
Travless <90566281+Travless@users.noreply.github.com>
2023-03-11 17:38:48 -0500

    Create initial global error handling middleware that will return the error status code and status from a universal standpoint within the app

--- app.js
+++ app.js
@@ -25,12 +25,31 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // handler that will catch all requests that aren't caught by the designated tour and user handlers
+// Operational error
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}.`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl}.`,
+  // });
+  // next();
+
+  const err = new Error(`Can't find ${req.originalUrl}.`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  // Error code is either what is provided by the error or the default 500 (internal server error)
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
-  next();
 });
 
 module.exports = app;
