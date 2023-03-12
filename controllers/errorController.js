f2f9fe58869a97ce86d43feb7568c68f0b5d153d
Travless <90566281+Travless@users.noreply.github.com>
2023-03-12 14:08:19 -0400

    Provide error name and value to production requests

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable node/no-unsupported-features/es-syntax */
+/* eslint-disable prettier/prettier */
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -8,10 +18,21 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
+    });
+    // Programming or other unknown error: don't leak error details to client
+  } else {
+    // 1) Log error
+    console.error('ERROR', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Oops! Something went wrong!',
     });
   }
 };
@@ -25,9 +46,13 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODDE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = Object.create(err);
+
+    if(err.name === "CastError") error = handleCastErrorDB(err);
+
+    sendErrorProd(error, res);
   }
 };
