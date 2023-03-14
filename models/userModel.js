f194d44e9e822a0040ae048ad352fcad0cb29390
Travless <travis.white0101@gmail.com>
2023-03-14 14:53:03 -0400

    assign freshUser to req.user in the protect middleware to get a fresh user for the next middleware

--- models/userModel.js
+++ models/userModel.js
@@ -76,8 +76,6 @@ userSchema.methods.changedPasswordAfter = async function (JWTTimestamp) {
       this.passwordChangedAt.getTime() / 1000,
       10
     );
-
-    console.log(changedTimestamp, JWTTimestamp);
     return JWTTimestamp < changedTimestamp;
   }
 
