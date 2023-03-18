6cfe8645e624ce5a411bbbde5a32470ed313d263
Travless <travis.white0101@gmail.com>
2023-03-18 18:45:59 -0400

    Implement a rate limiter on all API routes to begin increasing security measures on attacks like denail of service or brute

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit= require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHAndler = require('./controllers/errorController');
@@ -8,10 +9,18 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// set limiter for additional security for such things like denial of service or brute attacks
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${this._router}/public`));
