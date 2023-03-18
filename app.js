8ad12e15a5631181a25d6c05fe3fa3b39739fb88
Travless <travis.white0101@gmail.com>
2023-03-18 19:18:12 -0400

    Add additional middleware for security validation via npm packages like xss clean and mongoSanitize to protect against NoSQL query injections

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit= require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHAndler = require('./controllers/errorController');
@@ -31,6 +33,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from the body into req.body, limits body size to 10kb
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data santization against XSS (cross scripting attacks)
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${this._router}/public`));
