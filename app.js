fa400c19cf5396446875e67d2b2a99acb0cad199
Travless <travis.white0101@gmail.com>
2023-03-18 18:57:32 -0400

    Clean up some global mddleware functions within app.js so they are more organized. Also install and implement helmet.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit= require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHAndler = require('./controllers/errorController');
@@ -10,6 +12,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -20,11 +26,16 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour.',
 });
+// app.use() always needs a function, not a function call
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the body into req.body, limits body size to 10kb
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${this._router}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
