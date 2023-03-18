f70087c3aba6299c30a4fd38865471fc928d7672
Travless <travis.white0101@gmail.com>
2023-03-18 19:29:16 -0400

    Implement middleware to prevent parameter pollution via npm package hpp

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const rateLimit= require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
+
 
 const AppError = require('./utils/appError');
 const globalErrorHAndler = require('./controllers/errorController');
@@ -39,6 +41,20 @@ app.use(mongoSanitize());
 
 // Data santization against XSS (cross scripting attacks)
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving static files
 app.use(express.static(`${this._router}/public`));
