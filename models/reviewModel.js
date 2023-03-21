29f0caefeedd6361765105db4e72855b3407c5de
Travless <90566281+Travless@users.noreply.github.com>
2023-03-21 08:43:30 -0400

    Create basis of reviewSchema for reviews to be modeled from, including references to tours and users

---
+++ models/reviewModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+// review, rating, createdAt, ref to tour, ref to user
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'A review must have content.'],
+    },
+    rating: {
+      type: Number,
+      min: [1, 'Rating must be greater than or equal to 1.'],
+      max: [5, 'Rating must be less than or equal to 5.'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'Review must belong to a tour.'],
+      },
+    ],
+    user: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        require: [true, 'Review must have a user'],
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
