bfdea7977dec41c0dfc4683ed326c13efe8496bd
Travless <90566281+Travless@users.noreply.github.com>
2023-03-24 12:13:53 -0400

    Create getToursWithin get method with gathers tours within a certain provided radius based on a geospacial query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -118,17 +118,26 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
 // // query strings  /tours-distance?distance=233&center=-40,45&unit=mi
 // //  /tours-distance/233/center/34.111745,-118.113491/unit/mi
 
-exports.getToursWithin = (req, res, next) => {
+exports.getToursWithin = catchAsync( async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
 
   if (!lat || !lng) {
     next(new AppError('Please provide latitude and longitude in the format lat,lng.', 400));
   }
 
-  console.log(distance, lat, lng, unit);
+  const tours = await Tour.find({ 
+    // define longitude first then latitude
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } } 
+  })
 
   res.status(200).json({
     status: 'success',
+    results: tours.length,
+    data: {
+      data: tours,
+    }
   })
-};
+});
