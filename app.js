b8923cc55e2c6f0fd97c650883ac2596300abd25
Travless <travis.white0101@gmail.com>
2023-03-28 13:30:25 -0400

    Restrcuture pug overview to loop through tours array and generate tour cards based oon the array's length

--- app.js
+++ app.js
@@ -74,18 +74,6 @@ app.use((req, res, next) => {
 
 // 2) ROUTES
 
-app.get('/', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
