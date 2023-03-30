706f55cb53c193e330171bbe6d2d6f1bb21c7d52
Travless <travis.white0101@gmail.com>
2023-03-30 14:20:44 -0400

    Implement mapbox library within the head of the base pug file and edit functionality via the mapbox.js file

---
+++ public/js/mapbox.js
@@ -0,0 +1,39 @@
+/*eslint-disable*/
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJhdmxlc3MiLCJhIjoiY2xmdmN1aXF2MDV1cTNwczNyaGt1d2NjOSJ9.TZ3OiFZvnmJF2gCCvC2d6g';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/travless/clfvf4vph00lm01mxevslngjv',
+  scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+    // Add marker
+    new mapboxgl.Marker({
+        element: el,
+        ancchor: 'bottom'
+    }).setLngLat(loc.coordinates).addTo(map);
+    // Add popup
+    new mapboxgl.Popup({
+        offset: 30
+    }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`).addTo(map);
+    // Extend the map bounds to include current location
+    bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+    padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100
+    }
+});
