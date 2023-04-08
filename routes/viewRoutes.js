7dee4a80a332b5972d8d1c08c7afaf9c601f7dda
Travless <90566281+Travless@users.noreply.github.com>
2023-04-08 10:59:19 -0400

    add isLoggedIn middleware to view routes that aren't account protected. Also create getAccount function to render Account pug page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,9 +5,9 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/', authController.isLoggedIn, viewController.getOverview);
-
-router.get('/', viewController.getOverview);
-router.get('/tour/:slug', viewController.getTour);
-router.get('/login', viewController.getLoginForm);
+router.get('/', authController.isLoggedIn, viewController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
+router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
+router.get('/me', authController.protect, viewController.getAccount);
 
 module.exports = router;
