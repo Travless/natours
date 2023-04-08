81ae01fe147eddc7fd921279f785c46ac37243dd
Travless <90566281+Travless@users.noreply.github.com>
2023-04-08 12:25:19 -0400

    Create ability to update user data, specifically name and email, through a POST request in account pub file

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ import { showAlert } from './alert';
 export const login = async (email, password) => {
     try {
         const res = await axios({
-            method: 'Post',
+            method: 'POST',
             url: 'http://127.0.0.1:3000/api/v1/users/login',
             data: {
                 email,
