989a816424ce4efd511258a3fa992649d512975d
Travless <90566281+Travless@users.noreply.github.com>
2023-04-09 17:37:15 -0400

    Fix bug in which when updating user password on the account pagee, the password is being updated on a different account, specifically the admin account

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,55 @@
 /* eslint-disable */
 import 'core-js/stable';
 import { displayMap } from './mapbox';
-import { login, logout } from "./login";
-import { updateData } from './updateSettings';
+import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
-// DOM Elements
+// DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
-const logoutBtn = document.querySelector('.nav__el.nav__el--logout');
-const saveSettings = document.querySelector('.form-user-data');
+const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-settings');
 
-// Delegation
-if(mapBox){
-    const locations = JSON.parse(document.getElementById('map').dataset.locations);
-    displayMap(locations);
-};
+// DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
 
-if(loginForm){
-    loginForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        login(email, password);
-    })
-};
+if (loginForm)
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
 
-if(logoutBtn) {
-    logoutBtn.addEventListener('click', logout);
-};
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if(saveSettings) {
-    saveSettings.addEventListener('submit', e => {
-        e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        updateData(name, email);
-    })
-};
+if (userDataForm)
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    // document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    // document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
