fa7f9918e5942d1047350646651f3bd729391d23
Travless <travis.white0101@gmail.com>
2023-04-10 13:36:39 -0400

    User able to successfully change password without it redirecting to admin's account and making changes on that account and not the user's account

--- models/userModel.js
+++ models/userModel.js
@@ -1,52 +1,42 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const validator = require('validator');
 const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A user must have a name.'],
-    maxLength: [
-      40,
-      "a user's name must be less than or equal to 40 characters",
-    ],
-    minLength: [10, "A user's name must have at least 10 or more characters."],
+    required: [true, 'Please tell us your name!']
   },
   email: {
     type: String,
-    required: [true, 'A user must provide an email address.'],
+    required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email.'],
+    validate: [validator.isEmail, 'Please provide a valid email']
   },
   photo: String,
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
-    default: 'user',
+    default: 'user'
   },
   password: {
     type: String,
-    required: [true, 'A user must provide a password.'],
-    minLength: [
-      10,
-      "A user's password must be at least 10 or more characters.",
-    ],
-    select: false,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'You must confirm your provided password.'],
+    required: [true, 'Please confirm your password'],
     validate: {
-      // Use standard function format, not arrow function, so you have access to this keyword
-      // This validator only works on CREAT and SAVE!
-      validator: function (el) {
+      // This only works on CREATE and SAVE!!!
+      validator: function(el) {
         return el === this.password;
       },
-      message: 'Passwords are not the same',
-    },
+      message: 'Passwords are not the same!'
+    }
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
@@ -54,53 +44,49 @@ const userSchema = new mongoose.Schema({
   active: {
     type: Boolean,
     default: true,
-    select: false,
-  },
+    select: false
+  }
 });
 
-// Encryption middleware
-// Happens between receiving the data and when it presisted to the database (prehook)
-userSchema.pre('save', async function (next) {
-  // Only run ths function if password if password was actually modified
+userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
 
-  // bycrypt creates hash based on cost parma that is passed into hash function and generates salt based on it
+  // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
 
-  // Delete password confirm fieldS
+  // Delete passwordConfirm field
   this.passwordConfirm = undefined;
   next();
 });
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', function(next) {
   if (!this.isModified('password') || this.isNew) return next();
 
-  // set passwordChangedAt to one sec before so that the token is always generated after the password has been changed in case adding to DB is slowed
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
 
-userSchema.pre(/^find/, function (next) {
+userSchema.pre(/^find/, function(next) {
   // this points to the current query
   this.find({ active: { $ne: false } });
   next();
 });
 
-// check if provided password is the same as encrypted password via instance method
-// instance method is available on all user documents
-userSchema.methods.correctPassword = async function (
+userSchema.methods.correctPassword = async function(
   candidatePassword,
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
   if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(
       this.passwordChangedAt.getTime() / 1000,
       10
     );
+
     return JWTTimestamp < changedTimestamp;
   }
 
@@ -108,12 +94,9 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   return false;
 };
 
-userSchema.methods.createPasswordResetToken = function () {
-  // Use crypto util to encrypt passwordResetToken
-  // crypto.randomBytes(desired length of token).toString('hex', convert encrypted string to hexagonal string)
+userSchema.methods.createPasswordResetToken = function() {
   const resetToken = crypto.randomBytes(32).toString('hex');
 
-  // use crypto util to set password reset token to user schema
   this.passwordResetToken = crypto
     .createHash('sha256')
     .update(resetToken)
@@ -121,10 +104,8 @@ userSchema.methods.createPasswordResetToken = function () {
 
   console.log({ resetToken }, this.passwordResetToken);
 
-  // this sets the passworrd reset token to expire in 10 minutes (formatted in miliseconds) from whan it's requested
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
-  // Plain text token that will be sent via email
   return resetToken;
 };
 
